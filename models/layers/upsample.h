6d4550112f99aa1f8fcb0a60bf14726186402196
bogil <bogilkim@yonsei.ac.kr>
2020-11-23 17:19:21 +0900

    update

---
+++ models/layers/upsample.h
@@ -0,0 +1,46 @@
+#ifndef __UPSAMPLE_LAYER_H__
+#define __UPSAMPLE_LAYER_H__
+
+#include "layer.h"
+
+namespace nebula {
+
+class upsample_layer_t : public layer_t {
+public:
+    upsample_layer_t(network_t *m_network, layer_t *m_prev_layer, layer_type_t m_layer_type);
+    ~upsample_layer_t();
+    
+    // Initialize layer.
+    void init(section_config_t m_section_config);
+    // Initialize weight from file.
+    void init_weight(std::fstream &m_weight_file);
+    // Initialize weight from scratch
+    void init_weight();
+    // Forward propagation.
+    void forward();
+    // Backward propagation.
+    void backward();
+    // Layer update
+    void update();
+    // Store weight.
+    void store_weight(std::fstream &m_weight_file);
+
+#ifdef GPU_ENABLED
+    // Forward propagation
+    void _forward_();
+    // Backward propagation
+    void _backward_();
+    // Layer update
+    void _update_();
+#endif
+
+private:
+    unsigned stride;
+    float scale;
+
+};
+
+}
+// End of namespace nebula.
+
+#endif
