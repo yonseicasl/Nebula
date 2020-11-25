8c42d6160602e1e46a48a59fb7532c395ff13a24
bogil <bogilkim@yonsei.ac.kr>
2020-11-25 13:37:16 +0900

    Fix error on dataset.sh

--- dataset.sh
+++ dataset.sh
@@ -41,17 +41,17 @@ fi
 data_ID() {
 	case $dataset in 
 		imagenet_large )
-            DATAID="1FAOrcFcpRbR0mMaWv5vzUq-PeyIJHsFv" ;;
+            DATAID="1qUzFfKqihTVsH9kds0taYYu2Vc7lrIcs" ;;
 		imagenet_medium )
-            DATAID="1UeFJa35DOhwR_iaxbX15_e30ETFSo4O8" ;;
+            DATAID="1RspmHUeV3qyOYnqtbramv61mCDiu1B7r" ;;
 		imagenet_small )
-			DATAID="1zCTB82sCmBcf0tCapOa0cd7tAcPp9ADe" ;;
+			DATAID="1ueT5X7ICjuBuC9DkVGWm8j6u7hWakCNn" ;;
 		nist_large )
-			DATAID="1BiZkpBSoT562Qfacy95KHP7Z1-MoZwX8" ;;
+			DATAID="1TO1cIXUQtNeb3InQf4iDV9hDbA97MJGx" ;;
 		nist_medium )
-			DATAID="1grcL1Ktahh1dgUbKqLIOv7BR7hzJmjo8" ;;
+			DATAID="1d_Wue5Dype3KucgWZnqQbhqGcGd20o8E" ;;
 		nist_small )
-			DATAID="1pRW2Uovd8r1Fd-qKb6mMjFbFnXGd-JMu" ;;
+			DATAID="1ufIjaGJTJNdyAdY-jZkx9PUSshqYeEIX" ;;
 		ptb_large )
             DATAID="1ErHMRMyxTNqaEKk4tuPrq-f-EVpW0enK" ;;
 		ptb_medium )
