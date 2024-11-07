3816e28aef7060dc4ed83e4d432e6569920b21d1
bogilkim <bogilkim@yonsei.ac.kr>
2024-11-07 11:00:45 +0900

    update weight.sh

--- weight.sh
+++ weight.sh
@@ -44,35 +44,61 @@ weight_ID() {
             RLKEY="3c4y0yuc6366c1e6enj55o1bq"
             ;;
 		alexnet_medium )
-            FILEID="1MYFsiV-LHt4sK-OumUlbfWooOcsFze7Y" ;;
+            FILEID="ww49ab035d0a9dnr6a0yg"
+            RLKEY="0d6pyldbyyvve201gj8p6ysjd"
+            ;;
 		alexnet_small )
-            FILEID="1fjJPJnJ914w8BsS7yDe-m9Cre3KZVOZi" ;;
+            FILEID="xftc2qad6qzkoahpl7bps"
+            RLKEY="ldx036geqe9imw77a36abblci"
+            ;;
 		vgg_large )
-            FILEID="1IfKa3pgt5W9kMtj1OVeuV1bNW4jfxhlD" ;;
+            FILEID="7qtpkr5qgn419x4bfmhio"
+            RLKEY="b5b2ec277y3qq1264etnxthxi"
+            ;;
 		vgg_medium )
-            FILEID="19gUDTtHQInK12y0PUgyOPtxomkwlLJhl" ;;
+            FILEID="io1heo0u8ytbtrnelm98e"
+            RLKEY="xapo2k06q8mhd246vejikjysy"
+            ;;
 		vgg_small )
-            FILEID="1L6GzG0Je43jd6sVWICFC8oCVEtP507ee" ;;
+            FILEID="2adpc22v9cfu121wqngbn"
+            RLKEY="89j5mi9hvai16u9nzlrhutpuf"
+            ;;
 		mlp_large )
-			FILEID="1l8POYfrLT2ZEIRbljt-7ub7-dXpEA1l2" ;;
+			FILEID="v7hrjk3yslmihlkqnv0df"
+            RLKEY="5i4r8qlmwi4mre7nj9n652x7o"
+            ;;
 		mlp_medium )
-			FILEID="1Re0I0q0_ngt2NRcAd22AQthpxCtoeTi6" ;;
+			FILEID="oc1kf9d770hrdhqtuh8qb"
+            RLKEY="pxn0yv56ph4wbf853iockpmsp"
+            ;;
 		mlp_small )
-			FILEID="1Rvx5gzy-ActdGjHWWiz5uJTfV5AD-oQ_" ;;
+			FILEID="2afw3bq5m0hp0cszo6pk3"
+            RLKEY="2xc9ospokaxp3imqbunhsf6ts"
+            ;;
 		dbn_large )
-			FILEID="1b1Fhhxsn4SRTmjLtii6Bxmjh_i71bZmr" ;;
+			FILEID="qc5k6fw2071fb5kzcl36q"
+            RLKEY="az50l7os9dttgf5d1q7muz2rb"
+            ;;
 		dbn_medium )
-			FILEID="1dertTo4oNPxb8u4I3RVP9g1absIyAV78" ;;
+			FILEID="cmrjlqf27qsrfsosjkj6s"
+            RLKEY="7ou81c9t3cls8vzj7s96jwnrp"
+            ;;
 		dbn_small )
-			FILEID="1eT8bN0DPPtQNLumF92IEHQI31vTLV9gx" ;;
-		resnet50_large )
+			FILEID="3w2ner7n4peq8w9w3i7ij"
+            RLKEY="zhgxz461oesu1gjdquidm8026"
+            ;;
+		resnet_large )
 			FILEID="9iotd5dh3yenn1v1gonee" 
             RLKEY="mj4dfjtoyi7v353r44n8uxipk"
             ;;
-		resnet50_medium )
-            FILEID="1KzzvRJkYE4Qu5n7kjwBwbfKyAYYbROui" ;;
-		resnet50_small )
-            FILEID="1DlERgUr2dOPZbPUP16EZ7y2EWjxR0Qy5" ;;
+		resnet_medium )
+            FILEID="m6n4rpfi1tgzcrdw56zxb"
+            RLKEY="x12u1k60nqnlqov89iomtar4o"
+            ;;
+		resnet_small )
+            FILEID="92z9ygdf63zczwtxfxbb3"
+            RLKEY="9ow8xjuidm928ao4wnd5by4ro"
+            ;;
 		rnn_large )
 			echo -e "Does not support weight file of $1"
 			exit 1 ;;
@@ -102,4 +128,4 @@ weight_ID $network
 cd weights
 
 wget --no-check-certificate "https://www.dropbox.com/scl/fi/$FILEID/$network.wgh?rlkey=$RLKEY"
-mv $network.wgh?rlkey=$RLKEY input.wgh 
+mv $network.wgh?rlkey=$RLKEY benchmarks/$network/input.wgh 
